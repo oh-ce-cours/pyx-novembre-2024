7bdc6aa2e1e1d9316adbd2095285718e23c1b25b
Matthieu Falce <matthieu@falce.net>
2024-12-18 17:34:55 +0100

    gitautosync update 79

--- tape_en_cours/hello_world.py
+++ tape_en_cours/hello_world.py
@@ -3,12 +3,14 @@ import pandas
 print("hello world")
 print(1 == 2)
 
-for nombre in range(1, 101):
-    resultat = ""
-    if nombre % 3 == 0:
-        resultat = resultat + "fizz"
-    if nombre % 5 == 0:
-        resultat += "buzz"
-    if resultat == "":
-        resultat = nombre
-    print(resultat)
+
+def fizzbuzz() -> int:
+    for nombre in range(1, 101):
+        resultat = ""
+        if nombre % 3 == 0:
+            resultat = resultat + "fizz"
+        if nombre % 5 == 0:
+            resultat += "buzz"
+        if resultat == "":
+            resultat = nombre
+        return resultat
