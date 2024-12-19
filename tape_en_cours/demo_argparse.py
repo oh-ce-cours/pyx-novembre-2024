2cf32ac66d68865a06d249c5549827b4e2a0d9e7
Matthieu Falce <matthieu@falce.net>
2024-12-19 14:35:23 +0100

    gitautosync update 190

---
+++ tape_en_cours/demo_argparse.py
@@ -0,0 +1,16 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Additionne deux nombres.")
+    parser.add_argument("num1", type=float, help="Le premier nombre")
+    parser.add_argument("num2", type=float, help="Le deuxième nombre")
+
+    args = parser.parse_args()
+
+    result = args.num1 + args.num2
+    print(f"La somme de {args.num1} et {args.num2} est {result}")
+
+
+if __name__ == "__main__":
+    main()
