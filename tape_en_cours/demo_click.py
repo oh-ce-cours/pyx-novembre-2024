41756793b7d6cd4ebcc04a0c0a4ef474bad3812b
Matthieu Falce <matthieu@falce.net>
2024-12-19 14:35:35 +0100

    gitautosync update 191

---
+++ tape_en_cours/demo_click.py
@@ -0,0 +1,14 @@
+import click
+
+
+@click.command()
+@click.argument("num1", type=float)
+@click.argument("num2", type=float)
+def add(num1, num2):
+    """Additionne deux nombres."""
+    result = num1 + num2
+    print(f"La somme de {num1} et {num2} est {result}")
+
+
+if __name__ == "__main__":
+    add()
