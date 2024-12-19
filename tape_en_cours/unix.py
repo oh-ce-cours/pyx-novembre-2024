44c216047f0c0492d107ec3d7a868e0d1c25903e
Matthieu Falce <matthieu@falce.net>
2024-12-19 11:39:50 +0100

    gitautosync update 135

--- tape_en_cours/unix.py
+++ tape_en_cours/unix.py
@@ -2,5 +2,12 @@ f = open("./tape_en_cours/hello_world.py", encoding="utf8")
 lines = f.readlines()
 f.close()
 
-for line in lines[:10]:
-    print(line, end="")
+
+def head():
+    for line in lines[:10]:
+        print(line, end="")
+
+
+def tail():
+    for line in lines[-10:]:
+        print(line, end="")
