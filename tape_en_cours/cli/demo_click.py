42d1b2627a48aa64e3dac689fc66fa6bb8edf616
Matthieu Falce <matthieu@falce.net>
2024-12-19 14:43:52 +0100

    gitautosync update 210

--- tape_en_cours/cli/demo_click.py
+++ tape_en_cours/cli/demo_click.py
@@ -14,7 +14,7 @@ from utils import *
 @click.argument("num2", type=float)
 def add(num1, num2):
     """Additionne deux nombres."""
-    result = addition(num1, num2)
+    result = utils.addition(num1, num2)
     print(f"La somme de {num1} et {num2} est {result}")
 
 
