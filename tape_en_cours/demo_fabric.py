1d0536efae342a20b83ea5e2a676a1ca535f6816
Matthieu Falce <matthieu@falce.net>
2024-12-20 16:17:09 +0100

    gitautosync update 114

--- tape_en_cours/demo_fabric.py
+++ tape_en_cours/demo_fabric.py
@@ -1,5 +1,5 @@
 from fabric import Connection
 
-result = Connection("web1.example.com").run("uname -s", hide=True)
+result = Connection("51.210.243.135", "ubuntu").run("uname -s", hide=True)
 msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
 print(msg.format(result))
