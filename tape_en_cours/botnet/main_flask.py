25b907bf785be1ceb57b4bd96544afebb90430df
Matthieu Falce <matthieu@falce.net>
2024-12-20 14:51:50 +0100

    gitautosync update 38

--- tape_en_cours/botnet/main_flask.py
+++ tape_en_cours/botnet/main_flask.py
@@ -3,12 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/action", methods=["POST"])
+def action():
+    return {"salut": "les gars"}
+
+
 @app.route("/")
 @app.route("/<string:name>")
 def hello_world(name="default"):
     return {"salut": "les gars", "name": name}
-
-
-@app.route("/action", methods=["POST"])
-def action():
-    return {"salut": "les gars"}
