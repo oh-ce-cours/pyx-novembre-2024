4cf3a27a3beb7c70634bcbe72692b0fc7ceeb6ae
Matthieu Falce <matthieu@falce.net>
2024-12-20 15:05:34 +0100

    gitautosync update 50

--- tape_en_cours/botnet/client.py
+++ tape_en_cours/botnet/client.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.post(
-    "http://127.0.0.1:5000/action", json={"message": "Hello, world!"}
+    "http://127.0.0.1:5000/action", json={"message": "Hello, world!"}, timeout=1
 )
 response.raise_for_status()
 print(response.json())
