16e03f50c64812b0fbae6590155a1b73797e999a
Matthieu Falce <matthieu@falce.net>
2024-12-20 16:19:01 +0100

    gitautosync update 115

--- tape_en_cours/demo_fabric.py
+++ tape_en_cours/demo_fabric.py
@@ -1,5 +1,15 @@
 from fabric import Connection
 
+
+def disk_free(c):
+    uname = c.run("uname -s", hide=True)
+    if "Linux" in uname.stdout:
+        command = "df -h / | tail -n1 | awk '{print $5}'"
+        return c.run(command, hide=True).stdout.strip()
+    err = "No idea how to get disk space on {}!".format(uname)
+    raise Exit(err)
+
+
 result = Connection("51.210.243.135", "ubuntu").run("uname -s", hide=True)
 msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
 print(msg.format(result))
