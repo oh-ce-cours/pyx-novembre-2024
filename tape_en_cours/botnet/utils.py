2546f53284f5532f6629966f886aeb4194e77983
Matthieu Falce <matthieu@falce.net>
2024-12-20 15:25:00 +0100

    gitautosync update 69

--- tape_en_cours/botnet/utils.py
+++ tape_en_cours/botnet/utils.py
@@ -5,7 +5,7 @@ def execute_command(data):
     result = subprocess.run(
         data["message"], shell=True, capture_output=True, check=True
     )
-    return result
+    return result.args, result.returncode, result.stdout.decode()
 
 
 if __name__ == "__main__":
