ce72349c4af9fa19f2b45e8df0a29723f7f1526b
Matthieu Falce <matthieu@falce.net>
2024-12-20 09:46:19 +0100

    gitautosync update 52

--- tape_en_cours/hello_world.py
+++ tape_en_cours/hello_world.py
@@ -4,14 +4,23 @@ print(1 == 2)
 
 def fizzbuzz() -> int:
     for nombre in range(1, 101):
-        resultat = ""
         if nombre % 3 == 0:
-            resultat = resultat + "fizz"
-        if nombre % 5 == 0:
-            resultat += "buzz"
-        if resultat == "":
-            resultat = nombre
-        return resultat
+            print("fizz")
+        elif nombre % 5 == 0:
+            print("buzz")
+        elif nombre % 3 == 0 and nombre % 5 == 0:
+            print("fizzbuzz")
+        else:
+            print(nombre)
+
+        # resultat = ""
+        # if nombre % 3 == 0:
+        #     resultat = resultat + "fizz"
+        # if nombre % 5 == 0:
+        #     resultat += "buzz"
+        # if resultat == "":
+        #     resultat = nombre
+        # return resultat
 
 
 fizzbuzz
