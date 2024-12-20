8106582f65e475bf5143672f4bc2adca8c5289bc
Matthieu Falce <matthieu@falce.net>
2024-12-20 16:35:32 +0100

    gitautosync update 136

--- tape_en_cours/demo_multiprocess.py
+++ tape_en_cours/demo_multiprocess.py
@@ -3,10 +3,10 @@ from multiprocessing import Pool
 
 
 def compute(x):
-    time.sleep(10)
+    time.sleep(1)
     return x * x
 
 
 if __name__ == "__main__":
-    with Pool(10) as p:
+    with Pool(5) as p:
         print(p.map(compute, range(10)))
