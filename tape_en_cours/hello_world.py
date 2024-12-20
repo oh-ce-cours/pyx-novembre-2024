778df530645a5190239af03ca7275fcaf61d73f6
Matthieu Falce <matthieu@falce.net>
2024-12-20 12:16:04 +0100

    gitautosync update 1

--- tape_en_cours/hello_world.py
+++ tape_en_cours/hello_world.py
@@ -1,6 +1,16 @@
 print("hello world")
 print(1 == 2)
 
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="%(asctime)s %(message)s")
+logger.debug("This message should go to the log file")
+logger.info("So should this")
+logger.warning("And this, too")
+logger.error("And non-ASCII stuff, too, like Øresund and Malmö")
+logging.warning("%s before you %s", "Look", "leap!")
+
 
 def fizzbuzz():
     for nombre in range(1, 101):
