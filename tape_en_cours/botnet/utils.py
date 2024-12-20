7a229a55aaac01e03ec9481676ec15a887277249
Matthieu Falce <matthieu@falce.net>
2024-12-20 15:25:24 +0100

    gitautosync update 71

--- tape_en_cours/botnet/utils.py
+++ tape_en_cours/botnet/utils.py
@@ -2,12 +2,10 @@ import subprocess
 
 
 def execute_command(command):
-    result = subprocess.run(
-        data["message"], shell=True, capture_output=True, check=True
-    )
+    result = subprocess.run(command, shell=True, capture_output=True, check=True)
     return result.args, result.returncode, result.stdout.decode()
 
 
 if __name__ == "__main__":
-    result = execute_command({"command": "ls -la"})
+    result = execute_command("ls -la")
     print(result)
