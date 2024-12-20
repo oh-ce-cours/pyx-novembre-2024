83c8161d64fde1762a52869a511614df22137fc0
Matthieu Falce <matthieu@falce.net>
2024-12-20 15:45:31 +0100

    gitautosync update 90

--- tape_en_cours/botnet/demo_fastapi.py
+++ tape_en_cours/botnet/demo_fastapi.py
@@ -1,7 +1,15 @@
 import utils
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 @app.get("/actions/")
