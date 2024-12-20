fbb99719177e367d01f71f95b93d732fc453247c
Matthieu Falce <matthieu@falce.net>
2024-12-20 12:17:16 +0100

    gitautosync update 2

---
+++ tape_en_cours/poo.py
@@ -0,0 +1,12 @@
+class Personne:
+    def __init__(self, name, age, sexe=None):
+        print("dans le __init__")
+        self.name = name
+        self.age = age
+        self.sexe = sexe
+
+    def presenter(self):
+        return f"Je suis {self.name} et j'ai {self.age} ans et suis de sexe {self.sexe}"
+
+    def __str__(self):
+        return self.presenter()
