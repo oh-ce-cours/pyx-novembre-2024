e5d37e83ce82af0df993c97b5d5d630387c26aab
Matthieu Falce <matthieu@falce.net>
2024-12-20 15:48:05 +0100

    gitautosync update 98

--- tape_en_cours/botnet/client.py
+++ tape_en_cours/botnet/client.py
@@ -2,6 +2,6 @@ import requests
 import rich
 
 data = {"commande": "ls -alh"}
-response = requests.post("http://127.0.0.1:5000/action", json=data, timeout=1)
+response = requests.post("http://127.0.0.1:8000/action", json=data, timeout=1)
 response.raise_for_status()
 rich.print(response.json())
