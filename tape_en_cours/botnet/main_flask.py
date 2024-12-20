2e2d8ab5ec31d81d17331eb99bcf8c48a8e46c73
Matthieu Falce <matthieu@falce.net>
2024-12-20 14:46:26 +0100

    gitautosync update 31

--- tape_en_cours/botnet/main_flask.py
+++ tape_en_cours/botnet/main_flask.py
@@ -3,6 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/")
 @app.route("/<string:name>")
-def hello_world(name):
+def hello_world(name="default"):
     return f"<p>Hello {name}!</p>"
