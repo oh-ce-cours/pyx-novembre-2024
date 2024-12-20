afdc9d2c6c475968c981cd6a26957f8d66280b65
Matthieu Falce <matthieu@falce.net>
2024-12-20 15:07:06 +0100

    gitautosync update 51

--- tape_en_cours/botnet/main_flask.py
+++ tape_en_cours/botnet/main_flask.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/action", methods=["POST"])
 def action():
+    data = request.get_json()
     return {"salut": "les gars"}
 
 
