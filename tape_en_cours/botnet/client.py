46482624bee06983589a93255b103ab29693f1c7
Matthieu Falce <matthieu@falce.net>
2024-12-20 15:29:45 +0100

    gitautosync update 77

--- tape_en_cours/botnet/client.py
+++ tape_en_cours/botnet/client.py
@@ -1,6 +1,7 @@
 import requests
+import rich
 
 data = {"command": "ls -la"}
 response = requests.post("http://127.0.0.1:5000/action", json=data, timeout=1)
 response.raise_for_status()
-print(response.json())
+rich.print(response.json())
