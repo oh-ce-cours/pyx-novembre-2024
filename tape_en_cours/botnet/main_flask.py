4d093143396f88eb2abeafb8a1e5304550e92a9e
Matthieu Falce <matthieu@falce.net>
2024-12-20 14:50:46 +0100

    gitautosync update 36

--- tape_en_cours/botnet/main_flask.py
+++ tape_en_cours/botnet/main_flask.py
@@ -9,6 +9,6 @@ def hello_world(name="default"):
     return {"salut": "les gars", "name": name}
 
 
-@app.route("/action", methods=["POST"])
-def action(name="default"):
-    return {"salut": "les gars", "name": name}
+@app.route("/action")
+def action():
+    return {"salut": "les gars"}
