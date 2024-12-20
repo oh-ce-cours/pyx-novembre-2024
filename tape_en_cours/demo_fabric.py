19098017c1569de4af1356e37c1cf859861fca71
Matthieu Falce <matthieu@falce.net>
2024-12-20 16:21:20 +0100

    gitautosync update 120

--- tape_en_cours/demo_fabric.py
+++ tape_en_cours/demo_fabric.py
@@ -1,7 +1,7 @@
 from fabric import SerialGroup
 
 # result = Connection(", "ubuntu").run("uname -s", hide=True)
-result = SerialGroup("51.210.243.135", "146.59.156.185", "ubuntu").run("hostname")
+result = SerialGroup("51.210.243.135", "146.59.156.185", user="ubuntu").run("hostname")
 for item in result.items():
     print(item)
 # msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
